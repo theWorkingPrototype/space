c8ae9d18bc979e2d1787e46a6d0110b0b158ebb5
Aryan Srivastava <81866147+theWorkingPrototype@users.noreply.github.com>
2021-09-13 05:56:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -61,7 +61,7 @@ function init() {
         console.log("press");
         if (text[1] == " ") {
             score += 2 * (waitDelay - (performance.now() - t));
-            score=parseInt(score);
+            score=Math.floor(score);
             cancelanimate(animebox);
             gameLoop();
         }
